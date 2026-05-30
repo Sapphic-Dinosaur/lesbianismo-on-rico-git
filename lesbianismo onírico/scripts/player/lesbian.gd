cf04ccc92931a899d756b5a347a726fe740e0678
Sapphic-Dinosaur <hoazinnugget@gmail.com>
2026-05-29 21:43:34 -0600

    full 3d movement

--- lesbianismo onírico/scripts/player/lesbian.gd
+++ lesbianismo onírico/scripts/player/lesbian.gd
@@ -1,4 +1,5 @@
 extends CharacterBody3D
+class_name Player
 
 @onready var yaw: Marker3D = $Yaw
 @onready var pitch: Marker3D = $Yaw/Pitch
@@ -8,17 +9,33 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("play_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	_movement()
 	move_and_slide()
+
+func _movement():
+	var dir_input: Vector2 = Input.get_vector("play_left", "play_right", "play_forward", "play_back")
+	var direction: Vector3 = (yaw.transform.basis * Vector3(dir_input.x, 0, dir_input.y))
+	direction.x = abs(direction.x) * direction.x
+	direction.z = abs(direction.z) * direction.z
+	if direction: velocity = Vector3(direction.x * SPEED, velocity.y, direction.z * SPEED)
+	else: velocity = Vector3(0, velocity.y, 0)
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseMotion:
-		pitch.rotate_x(-event.relative.y * cam_sense)
-		yaw.rotate_y(-event.relative.x * cam_sense)
+		pitch.rotate_x(-event.relative.y * cam_sense * 0.001)
+		yaw.rotate_y(-event.relative.x * cam_sense * 0.001)
 		pitch.rotation.x = clamp(pitch.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _capture_mouse(pause: bool):
+	if !pause: Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else: Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
