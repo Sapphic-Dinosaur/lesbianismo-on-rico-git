d074e4b49bad201d80975013f1db1562770c47a1
Sapphic-Dinosaur <hoazinnugget@gmail.com>
2026-06-07 00:40:02 -0600

    add moving platform w animplay

--- lesbianismo onírico/scenes/levels/proto_level.gd
+++ lesbianismo onírico/scenes/levels/proto_level.gd
@@ -2,6 +2,10 @@ extends Node
 
 @export var player: Player
 @export var respawn_point: Marker3D
+@export var animation: AnimationPlayer
+
+func _ready():
+	animation.play("moving platform")
 
 func _on_fallstopper_area_entered(area: Area3D) -> void:
 	player.global_position = respawn_point.global_position
